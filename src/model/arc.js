d4244334ba4090c3948f4d1de88e495f03f2f0f0
Andrea <andrea@wittrock.us>
2017-10-27 23:51:46 -0500

    Initial implementation of arc moving

--- src/model/arc.js
+++ src/model/arc.js
@@ -5,9 +5,104 @@
 import {Entity} from 'atp-active-record';
 import config from 'atp-config';
 
+const tableName = 'atpcomic_arcs';
+
 export default class Arc extends Entity
 {
     constructor() {
-        super('comic', 'atpcomic_arcs');
+        super('comic', tableName);
+    }
+
+    nextSortOrder(parentId) {
+        return new Promise((resolve, reject) => {
+            this.select("MAX(sortOrder) + 1 as nextSortOrder")
+                .groupBy("parentId")
+                .where({parentId})
+                .limit(1)
+                .list(false)
+                .then(result => {
+                    resolve(result.length > 0 ? result[0].nextSortOrder : 0);
+                })
+                .catch(reject);
+        });
+    }
+
+    removeFromParent(id) {
+        return new Promise((resolve, reject) => {
+            //Get the existing arc to get its parentId
+            this.getById(id)
+                .then(arc => {
+                    const parentId = arc.parentId;
+                    //Remove the arc from its parent
+                    this.where({id}).limit(1).update({parentId: null})
+                        .then(() => {
+                            //Decrement sortOrder for all higher siblings
+                            this.query(
+                                "update " + tableName +
+                                " set sortOrder = sortOrder - 1" +
+                                " where parentId=" + parentId +
+                                " and sortOrder>" + arc.sortOrder +
+                                " and id<>" + id
+                            ).then(() => {
+                                this.getSiblings(parentId, id)
+                                    .then(siblings => {
+                                        resolve(siblings);
+                                    }).catch(reject);
+                            }).catch(reject);
+                        }).catch(reject);
+                }).catch(reject);
+        });
+    }
+
+    getSiblings(parentId, id) {
+        return this.select(['id', 'sortOrder'])
+            .where({parentId}).where("id <> " + id)
+            .list()
+    }
+
+    insertInto(parentId, id) {
+        return new Promise((resolve, reject) => {
+            this.where({id})
+                .limit(1)
+                .update({parentId, sortOrder: 0})
+                .then(() => {
+                    this.query(
+                        "update " + tableName +
+                        " set sortOrder = sortOrder + 1" +
+                        " where parentId=" + parentId +
+                        " and id<>" + id
+                    ).then(() => {
+                        this.getSiblings(parentId, id)
+                            .then(siblings => {
+                                resolve(siblings);
+                            }).catch(reject);
+                    }).catch(reject);
+                }).catch(reject);
+        });
+    }
+
+    insertAfter(targetId, id) {
+        return new Promise((resolve, reject) => {
+            this.getById(targetId)
+                .then(targetArc => {
+                    const parentId = targetArc.parentId;
+                    const sortOrder = targetArc.sortOrder;
+                    this.query(
+                        "update " + tableName +
+                        " set sortOrder = sortOrder + 1" +
+                        " where parentId=" + parentId +
+                        " and sortOrder>=" + sortOrder +
+                        " and id<>" + id
+                    ).then(() => {
+                        this.where({id}).limit(1).update({parentId, sortOrder})
+                            .then(() => {
+                                this.getSiblings(parentId, id)
+                                    .then(siblings => {
+                                        resolve(siblings);
+                                    }).catch(reject);
+                            }).catch(reject);
+                    }).catch(reject);
+                }).catch(reject);
+        })
     }
 }
--- src/routes/arc.js
+++ src/routes/arc.js
@@ -5,9 +5,40 @@
 import {basicController} from 'atp-rest';
 import {createCrudPermissions} from "atp-rest-uac";
 import Arc from "../model/arc";
+import {o} from 'atp-sugar';
+import {respondWith} from 'atp-rest';
 
 const permissions = createCrudPermissions('comic', 'arc');
 const model = Arc;
 const idField = 'arcId';
 
-export default basicController.entity.crud({model, permissions, idField});
+export default o(basicController.entity.crud({model, permissions, idField})).as(crud => o(crud).merge({
+    post: (req, res) => {
+        new Arc().nextSortOrder(req.body.parentId).then(sortOrder => {
+            req.body.sortOrder = sortOrder;
+            crud.post(req, res);
+        });
+    },
+    move: {
+        post: (req, res) => {
+            const action = req.body.action;
+            const targetId = req.body.targetId;
+            const sourceId = req.body.sourceId;
+            //TODO:  Add validation so that an arc can't be moved into one of its descendants (no loops)
+            new Arc().removeFromParent(sourceId).then(oldSiblings => {
+                o(action).switch({
+                    into: () => new Arc().insertInto(targetId, sourceId),
+                    after: () => new Arc().insertAfter(targetId, sourceId),
+                    default: () => {throw "Invalid move mode " + mode;}
+                }).then(newSiblings => {
+                    new Arc()
+                        .select(['id', 'parentId', 'sortOrder'])
+                        .getById(sourceId)
+                        .then(sourceArc => {
+                            respondWith.Success(req, res)(oldSiblings.concat(newSiblings, sourceArc))
+                        });
+                });
+            })
+        }
+    }
+}).raw);
