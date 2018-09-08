7a7e21ddf76d22a9dce9d537fb2fb341020fa0e5
Andrew Wittrock <andy@wittrock.us>
2018-09-07 19:24:18 -0500

    Add RSS feed for pages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 
 import arc from './routes/arc';
 import page from "./routes/page";
+import feed from "./routes/feed";
 import commentary from "./routes/commentary";
 import character from "./routes/character";
 import attribute from "./routes/attribute";
 
 export default ({
-    routes: {arc, attribute, page, commentary, character},
+    routes: {arc, attribute, page, feed, commentary, character},
     config: {
         //validators
     }
---
+++ src/routes/feed.js
@@ -0,0 +1,50 @@
+
+import Page from "../model/page";
+import {File} from "atp-rest-media";
+import {merge} from 'atp-pointfree';
+
+const feed = {
+    title: "Darkwynd Chronicles",
+    url: "http://api.darkwyndchronicles.com/feed",
+    ttl: "1800",
+    pageUrl: url => `http://www.darkwyndchronicles.com/comic/page/${url}`
+};
+
+export default {
+    get: (req, res) => {
+        const filters = {
+            perPage: 10,
+            sort: "postDate DESC"
+        };
+        new Page().filter(filters).list().then(resultsRaw => Promise.all(
+            resultsRaw.map(page => new File()
+                .getById(page.imageId)
+                .then(image => merge({image}, page))
+            ))
+        )
+        .then(results => {
+            res.setHeader('Content-Type', 'text/xml');
+            res.status(200);
+            res.send(`<?xml version="1.0" encoding="UTF-8" ?>
+                <rss version="2.0">
+                    <channel>
+                        <title>${feed.title} RSS Feed</title>
+                        <description></description>
+                        <link>${feed.url}</link>
+                        <lastBuildDate>${new Date()}</lastBuildDate>
+                        <pubDate>${new Date()}</pubDate>
+                        <ttl>${feed.ttl}</ttl>
+                        ${results.map(item => `
+                            <item>
+                                <title>${feed.title}: ${item.name}</title>
+                                <description>&lt;img src="https://media.darkwyndchronicles.com/${item.image.s3Prefix ? `${item.image.s3Prefix} - ` : ''}${item.image.fileName} - 512x512.${item.image.fileExtension}" /&gt;</description>
+                                <link>${feed.pageUrl(item.url)}</link>
+                                <pubDate>${item.postDate}</pubDate>
+                            </item>
+                        `).join("")}
+                    </channel>
+                </rss>
+            `);
+        });
+    }
+};
